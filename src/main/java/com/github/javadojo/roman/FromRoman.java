ebbb0e004e3d996fa2bc754a4eb4059fdf3968e3
Uwe Plonus <u.plonus@gmail.com>
2015-04-14 21:09:41 +0200

    Initial commit.

---
+++ src/main/java/com/github/javadojo/roman/FromRoman.java
@@ -0,0 +1,19 @@
+package com.github.javadojo.roman;
+
+/**
+ * A class with a method to convert a Roman numeral into an integer.
+ */
+public class FromRoman {
+
+    /**
+     * Converts the given {@code romanValue} in the range from {@code I}
+     * ({@code 1}) to {@code MMMCMXCIX} ({@code 3999}) into an integer.
+     *
+     * @param romanValue the {@code value} to convert.
+     * @return the integer.
+     */
+    public int fromRoman(String romanValue) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
